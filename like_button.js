c9cbe6c17764e51f10d898b0f1d4bc3fa0e2d7b6
Alessa Cross <alessacross@AlessanoMacBook-puro.local>
2018-09-20 18:54:48 +0900

    test

--- like_button.js
+++ like_button.js
@@ -13,13 +13,7 @@ class LikeButton extends React.Component {
       return 'Clicked about me.';
     }
 
-    return e(    <div id="top_bar">
-     <a href="education.html">Education</a>
-     <a href="experience.html">Experience</a>
-     <a href="projects.html">Projects</a>
-     <a href="resume.pdf" target="_blank">Resume</a>
-     <a href="contact_me.html">Contact Me</a>
-    </div>
+    return e(
       'button',
       { onClick: () => this.setState({ liked: true }) },
       'About Me'
